c2337ffffbf147cd6e2e9b694500179769ee5a00
jj0526 <wjstlgus1@naver.com>
2023-05-16 15:48:37 +0900

    edited

--- LeGO-LOAM/src/pcl_instantiate.cpp
+++ LeGO-LOAM/src/pcl_instantiate.cpp
@@ -2,8 +2,8 @@
 #include <pcl/filters/filter.h>
 #include "utility.h"
 
+template class pcl::PointCloud<PointXYZIGround>;  // Move this line before the class definition
+
 using PointCloudXYZIGround = pcl::PointCloud<PointXYZIGround>;
 
 template void removeNaNFromPointCloud<PointXYZIGround>(const PointCloudXYZIGround&, PointCloudXYZIGround&, std::vector<int>&);
-
-template class PointCloudXYZIGround;
